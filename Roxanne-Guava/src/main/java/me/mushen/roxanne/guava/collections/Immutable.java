bcbadae31f9749e0d72011a43ab7eea8a6f490b6
Rmushen <mushenrs@163.com>
2018-07-14 15:50:42 +0800

    init Project: Guava

---
+++ Roxanne-Guava/src/main/java/me/mushen/roxanne/guava/collections/Immutable.java
@@ -0,0 +1,12 @@
+package me.mushen.roxanne.guava.collections;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Desc
+ * @Author Mushen
+ * @Create 2018-07-14
+ */
+@Slf4j
+public class Immutable {
+}
